820ed066dbac0786c52f757f0a3c52cf1e84f884
jihoon <leejh080628@naver.com>
2026-02-21 19:57:56 +0900

    디자인시스템

---
+++ Moget/Moget/Color+DesignSystem.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+extension Color {
+    enum DS {
+        enum Purple {
+            static let light        = Color("Colors/Purple/light")
+            static let lightHover   = Color("Colors/Purple/lightHover")
+            static let lightActive  = Color("Colors/Purple/lightActive")
+            static let normal       = Color("Colors/Purple/normal")
+            static let normalHover  = Color("Colors/Purple/normalHover")
+            static let normalActive = Color("Colors/Purple/normalActive")
+            static let dark         = Color("Colors/Purple/dark")
+            static let darkHover    = Color("Colors/Purple/darkHover")
+            static let darkActive   = Color("Colors/Purple/darkActive")
+            static let darker       = Color("Colors/Purple/darker")
+        }
+        enum Gray {
+            static let white = Color("Colors/Gray/white")
+            static let g50   = Color("Colors/Gray/g50")
+            static let g100  = Color("Colors/Gray/g100")
+            static let g200  = Color("Colors/Gray/g200")
+            static let g300  = Color("Colors/Gray/g300")
+            static let g400  = Color("Colors/Gray/g400")
+            static let g500  = Color("Colors/Gray/g500")
+            static let g600  = Color("Colors/Gray/g600")
+            static let g700  = Color("Colors/Gray/g700")
+        }
+    }
+}
